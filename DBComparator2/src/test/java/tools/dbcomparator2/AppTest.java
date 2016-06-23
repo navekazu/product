f6846f5de49f428c86989b72150f7dfd3555eff4
navekazu <navekazu0927@gmail.com>
2016-06-23 17:15:13 +0900

    プロジェクト作成

---
+++ DBComparator2/src/test/java/tools/dbcomparator2/AppTest.java
@@ -0,0 +1,32 @@
+package tools.dbcomparator2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+    }
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
+}
