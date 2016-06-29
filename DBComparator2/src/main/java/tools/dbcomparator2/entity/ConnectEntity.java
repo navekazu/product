7469274f5a1d556c9f56c5690a74a40cc66c117d
navekazu <navekazu0927@gmail.com>
2016-06-29 11:59:03 +0900

    エンティティ作成

---
+++ DBComparator2/src/main/java/tools/dbcomparator2/entity/ConnectEntity.java
@@ -0,0 +1,25 @@
+package tools.dbcomparator2.entity;
+
+
+import lombok.Data;
+import lombok.experimental.Builder;
+
+@Data
+@Builder
+public class ConnectEntity {
+    public final String library;
+    public final String driver;
+    public final String url;
+    public final String user;
+    public final String password;
+
+    public static ConnectEntityBuilder copyBuilder(ConnectEntity entity) {
+        return ConnectEntity.builder()
+                .library(entity.library)
+                .driver(entity.driver)
+                .url(entity.url)
+                .user(entity.user)
+                .password(entity.password);
+    }
+
+}
---
+++ DBComparator2/src/test/java/tools/dbcomparator2/EntityTest.java
@@ -0,0 +1,49 @@
+package tools.dbcomparator2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import tools.dbcomparator2.entity.ConnectEntity;
+
+import static org.junit.Assert.assertEquals;
+
+public class EntityTest {
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+    }
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        ConnectEntity e = ConnectEntity.builder()
+                .library("a")
+                .driver("b")
+                .url("c")
+                .user("d")
+                .password("e")
+                .build();
+        ConnectEntity e2 = ConnectEntity.copyBuilder(e)
+                .user("f")
+                .build();
+
+        assertEquals("a", e2.getLibrary());
+        assertEquals("b", e2.getDriver());
+        assertEquals("c", e2.getUrl());
+        assertEquals("f", e2.getUser());
+        assertEquals("e", e2.getPassword());
+    }
+}
