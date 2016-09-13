c37e437f1b48aa63e807768314db8ac39ab3fb48
navekazu <navekazu0927@gmail.com>
2016-09-13 18:27:30 +0900

    Springのプロジェクト作成。

---
+++ ToDo/WebService/ToDoWebService/src/main/java/tools/todo/ToDoWebServiceApplication.java
@@ -0,0 +1,12 @@
+package tools.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoWebServiceApplication.class, args);
+	}
+}
