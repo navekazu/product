8bc9783bf9b25e5efb0f975c535a77dbc1dcfab9
navekazu <navekazu0927@gmail.com>
2016-09-30 16:36:54 +0900

    Entity追加。

---
+++ ToDo/WebService/ToDoWebService/src/main/java/tools/todo/entity/ToDo.java
@@ -0,0 +1,12 @@
+package tools.todo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Builder;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class ToDo {
+    private String title;
+}
