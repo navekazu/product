2423c78b143e8fa6df2ed71d4cb59f557438e106
navekazu <navekazu0927@gmail.com>
2016-10-05 15:34:36 +0900

    利用者クラスを追加。

---
+++ ToDo/WebService/ToDoWebService/src/main/java/tools/todo/entity/Owner.java
@@ -0,0 +1,4 @@
+package tools.todo.entity;
+
+public interface Owner {
+}
--- ToDo/WebService/ToDoWebService/src/main/java/tools/todo/entity/User.java
+++ ToDo/WebService/ToDoWebService/src/main/java/tools/todo/entity/User.java
@@ -7,8 +7,6 @@ import lombok.experimental.Builder;
 @Data
 @Builder
 @NoArgsConstructor
-public class ToDo {
-    private String title;
-    private ToDoGroup toDoGroup;
-    private Owner owner;
+public class User implements Owner {
+    private String name;
 }
