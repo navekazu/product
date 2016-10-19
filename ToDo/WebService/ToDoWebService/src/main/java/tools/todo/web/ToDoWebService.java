a7e60009779bb432e36a0ad14b91e8ca101beebf
navekazu <navekazu0927@gmail.com>
2016-10-19 11:11:47 +0900

    RESTのガワだけ実装。

--- ToDo/WebService/ToDoWebService/src/main/java/tools/todo/web/ToDoWebService.java
+++ ToDo/WebService/ToDoWebService/src/main/java/tools/todo/web/ToDoWebService.java
@@ -1,4 +1,15 @@
 package tools.todo.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tools.todo.entity.ToDo;
+
+@RestController
 public class ToDoWebService {
+    @RequestMapping("/todo")
+    public ToDo getToDo(@RequestParam(value="id") Long id) {
+        return null;
+    }
+
 }
