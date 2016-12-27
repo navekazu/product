4ba8ede05dc499e82a8eb6f249e406284b2d4257
navekazu <navekazu0927@gmail.com>
2016-12-27 18:16:30 +0900

    ロードしたライブラリから、特定のアノテーションが付いたクラスを抽出。

--- Mailer/src/main/java/tools/mailer/di/container/DIContainer.java
+++ Mailer/src/main/java/tools/mailer/di/container/DIContainer.java
@@ -3,17 +3,25 @@ package tools.mailer.di.container;
 import tools.mailer.di.anntation.Plugin;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.lang.instrument.Instrumentation;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class DIContainer {
     private static List<Plugin> pluginContainer;
@@ -62,7 +70,110 @@ public class DIContainer {
         String[] paths = property.split(System.getProperty("path.separator"));
         return Arrays.asList(paths).stream()
                 .filter(path -> Files.exists(Paths.get(path)))
-                .filter(path -> !Files.isDirectory(Paths.get(path)))
+//                .filter(path -> !Files.isDirectory(Paths.get(path)))
                 .collect(Collectors.toList());
+    }
+
+    List<String> getClassFiles(String file) {
+        if (file.endsWith(".jar")) {
+            return getClassFilesFromJarFile(file);
+        } else
+        if (file.endsWith(".zip")) {
+            return getClassFilesFromZipFile(file);
+        } else
+        if (Files.isDirectory(Paths.get(file))) {
+            return getClassFilesFromDirectory(file);
+        }
+        throw new IllegalArgumentException();
+    }
+
+    List<String> getClassFilesFromJarFile(String file) {
+        List<String> list = new ArrayList<>();
+
+        try (JarInputStream in = new JarInputStream(new FileInputStream(file))){
+            JarEntry entry;
+
+            while ((entry=in.getNextJarEntry())!=null) {
+                list.add(entry.getName());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list.stream()
+                .filter(fileName -> fileName.endsWith(".class"))
+                .collect(Collectors.toList());
+    }
+
+    List<String> getClassFilesFromZipFile(String file) {
+        List<String> list = new ArrayList<>();
+
+        try (ZipInputStream in = new ZipInputStream(new FileInputStream(file))){
+            ZipEntry entry;
+
+            while ((entry=in.getNextEntry())!=null) {
+                list.add(entry.getName());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list.stream()
+                .filter(fileName -> fileName.endsWith(".class"))
+                .collect(Collectors.toList());
+    }
+
+    List<String> getClassFilesFromDirectory(String directory) {
+        List<String> list = new ArrayList<>();
+
+        try {
+            try (Stream<Path> stream = Files.walk(Paths.get(directory))) {
+                stream.forEach(path -> list.add(path.toString()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list.stream()
+                .filter(fileName -> fileName.endsWith(".class"))
+                .map(fileName -> fileName.substring(directory.length()+1))      // パスの部分を削除してFQDNを作成
+                .collect(Collectors.toList());
+    }
+
+    String toFqdnClassName(String classFile) {
+        classFile = classFile.substring(0, classFile.length()-".class".length());
+        classFile = classFile.replace('/', '.');        // JarやZipなら/区切り
+        classFile = classFile.replace('\\', '.');       // directoryでwindowsなら\区切り
+        classFile = classFile.startsWith(".")? classFile.substring(1): classFile;
+        return classFile;
+    }
+
+    boolean isDeclaredAnnotations(String className, Class<? extends Annotation>[] targetAnnotations) {
+//        ClassLoader loader = DIContainer.class.getClassLoader();
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Class clazz = null;
+        try {
+            clazz = loader.loadClass(className);
+        } catch (NoClassDefFoundError e) {
+            return false;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+
+        Annotation[] annotations = clazz.getAnnotations();
+
+//        System.out.println("*****"+className+"*****");
+//        Arrays.asList(annotations).stream().forEach(System.out::println);
+        for (Annotation annotation: annotations) {
+            for (Class<? extends Annotation> targetAnnotation: targetAnnotations) {
+                if (annotation.annotationType().equals(targetAnnotation)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
--- Mailer/src/test/java/tools/mailer/di/container/DIContainerTest.java
+++ Mailer/src/test/java/tools/mailer/di/container/DIContainerTest.java
@@ -1,10 +1,15 @@
 package tools.mailer.di.container;
 
 import org.junit.*;
+import tools.mailer.di.anntation.Plugin;
 
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class DIContainerTest {
     @BeforeClass
@@ -36,4 +41,52 @@ public class DIContainerTest {
         jars.forEach(System.out::println);
         assertTrue(true);
     }
+
+    @Test
+    public void getClassFilesTest() throws Exception {
+        DIContainer diContainer = new DIContainer();
+        List<String> jars = diContainer.getLoadedJarFiles();
+        for (String jarFile: jars) {
+            System.out.println("jarFile=>"+jarFile);
+            diContainer.getClassFiles(jarFile)
+                    .forEach(System.out::println);
+        }
+        assertTrue(true);
+    }
+
+    @Test
+    public void toFqdnClassNameTest() throws Exception {
+        DIContainer diContainer = new DIContainer();
+        assertEquals("ccc", diContainer.toFqdnClassName("ccc.class"));
+        assertEquals("aaa.bbb.ccc", diContainer.toFqdnClassName("aaa/bbb/ccc.class"));
+        assertEquals("aaa.bbb.ccc", diContainer.toFqdnClassName("/aaa/bbb/ccc.class"));
+
+        List<String> jars = diContainer.getLoadedJarFiles();
+        List<String> classFiles = new ArrayList<>();
+        for (String jarFile: jars) {
+            classFiles.addAll(diContainer.getClassFiles(jarFile));
+        }
+        classFiles.stream()
+            .map(diContainer::toFqdnClassName)
+            .forEach(System.out::println);
+    }
+
+    @Test
+    public void isDeclaredAnnotationsTest() throws Exception {
+        DIContainer diContainer = new DIContainer();
+        List<String> jars = diContainer.getLoadedJarFiles();
+        List<String> classFiles = new ArrayList<>();
+        for (String jarFile: jars) {
+            classFiles.addAll(diContainer.getClassFiles(jarFile));
+        }
+
+        Class<? extends Annotation>[] targetAnnotations = new Class[]{Plugin.class};
+
+        List<String> annotationClasses = classFiles.stream()
+                .map(diContainer::toFqdnClassName)
+                .filter(className -> diContainer.isDeclaredAnnotations(className, targetAnnotations))
+                .collect(Collectors.toList());
+        annotationClasses.forEach(System.out::println);
+    }
+
 }
