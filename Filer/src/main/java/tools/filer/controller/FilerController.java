7419fb38ffa5da3f468176c1295b4271d6151041
navekazu <navekazu0927@gmail.com>
2017-03-31 10:39:51 +0900

    FilerInterfaceの実装。

--- Filer/src/main/java/tools/filer/controller/FilerController.java
+++ Filer/src/main/java/tools/filer/controller/FilerController.java
@@ -1,4 +1,26 @@
 package tools.filer.controller;
 
-public class FilerController {
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
+import tools.filer.FilerInterface;
+
+import java.nio.file.Path;
+
+public class FilerController implements FilerInterface {
+    @FXML
+    private TreeView directoryTree;
+
+    @FXML
+    private TableView fileListTable;
+
+    @Override
+    public Path getCurrentDirectory() {
+        return null;
+    }
+
+    @Override
+    public void setCurrentDirectory(Path path) {
+
+    }
 }
