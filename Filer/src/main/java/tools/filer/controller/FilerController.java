3974cfbb72cb95af8f4ca8ec9ee50e1ffdca784e
navekazu <navekazu0927@gmail.com>
2017-04-03 22:54:43 +0900

    コントローラとコマンドハンドラをhas-a関係に。

--- Filer/src/main/java/tools/filer/controller/FilerController.java
+++ Filer/src/main/java/tools/filer/controller/FilerController.java
@@ -1,18 +1,28 @@
 package tools.filer.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeView;
 import tools.filer.FilerInterface;
+import tools.filer.command.CommandHandler;
 
+import java.net.URL;
 import java.nio.file.Path;
+import java.util.ResourceBundle;
 
-public class FilerController implements FilerInterface {
+public class FilerController implements FilerInterface, Initializable {
     @FXML
     private TreeView directoryTree;
 
     @FXML
     private TableView fileListTable;
+
+    private CommandHandler commandHandler;
+
+    public FilerController() {
+        this.commandHandler = new CommandHandler(this);
+    }
 
     @Override
     public Path getCurrentDirectory() {
@@ -21,6 +31,11 @@ public class FilerController implements FilerInterface {
 
     @Override
     public void setCurrentDirectory(Path path) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
