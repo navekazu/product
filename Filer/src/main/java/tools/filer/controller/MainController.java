e988324924518caf3f72e5346da2494f29da1ae1
navekazu <navekazu0927@gmail.com>
2017-04-07 00:22:19 +0900

    パーツ用のフィールド宣言。

--- Filer/src/main/java/tools/filer/controller/MainController.java
+++ Filer/src/main/java/tools/filer/controller/MainController.java
@@ -1,4 +1,16 @@
 package tools.filer.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class MainController {
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField filterField;
+
+    @FXML
+    private TextField commandField;
+
 }
