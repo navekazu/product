9bd49f25169bc564c9c93c116a7ac2b288be9459
navekazu <navekazu0927@gmail.com>
2017-04-11 22:28:25 +0900

    ファイル選択とフィルタを定義。

--- Filer/src/main/java/tools/filer/FilerInterface.java
+++ Filer/src/main/java/tools/filer/FilerInterface.java
@@ -1,8 +1,11 @@
 package tools.filer;
 
 import java.nio.file.Path;
+import java.util.List;
 
 public interface FilerInterface {
     public Path getCurrentDirectory();
     public void setCurrentDirectory(Path path);
+    public void selectFile(List<Path> list);
+    public void filterFile(List<Path> list);
 }
