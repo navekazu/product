1f3cfccd5ca27454147224f7c6ab6014db3ef833
navekazu <navekazu0927@gmail.com>
2017-05-22 23:43:58 +0900

    コントリビュート一覧の取得。

--- GitHubContributeWatcher/src/main/java/tools/gitHubcontributewatcher/service/GitHubService.java
+++ GitHubContributeWatcher/src/main/java/tools/gitHubcontributewatcher/service/GitHubService.java
@@ -1,4 +1,9 @@
 package tools.githubcontributewatcher.service;
 
+import java.util.List;
+
 public class GitHubService {
+    public List<String> getContributeList() {
+        return null;
+    }
 }
