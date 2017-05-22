8e4d2381967daa32d6c06efd6278a3206fa35302
navekazu <navekazu0927@gmail.com>
2017-05-23 00:06:09 +0900

    コントリビュート一覧のテスト。

--- GitHubContributeWatcher/src/test/java/tools/gitHubcontributewatcher/controller/MainControllerTest.java
+++ GitHubContributeWatcher/src/test/java/tools/gitHubcontributewatcher/controller/MainControllerTest.java
@@ -1,7 +1,12 @@
 package tools.githubcontributewatcher.controller;
 
+import org.junit.Test;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MainControllerTest {
+    @Test
+    public void getContributeListTest() throws Exception {
+
+    }
 }
