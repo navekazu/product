287f8b1c097b8bc476bdb1034ad67f9809272095
navekazu <navekazu0927@gmail.com>
2017-05-29 00:22:02 +0900

    テスト実装中。

--- GitHubContributeWatcher/src/test/java/tools/gitHubcontributewatcher/service/GitHubServiceTest.java
+++ GitHubContributeWatcher/src/test/java/tools/gitHubcontributewatcher/service/GitHubServiceTest.java
@@ -2,10 +2,13 @@ package tools.githubcontributewatcher.service;
 
 import org.junit.Test;
 
+import java.util.List;
+
 public class GitHubServiceTest {
     @Test
     public void getContributeListTest() throws Exception {
         GitHubService service = new GitHubService();
+        List<String> list = service.getContributeList();
 
     }
 }
