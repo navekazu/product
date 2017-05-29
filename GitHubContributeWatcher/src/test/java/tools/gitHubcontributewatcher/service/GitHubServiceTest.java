0ed8ae21a0f3305136372751e40432b33faa97c8
navekazu <navekazu0927@gmail.com>
2017-05-30 06:53:25 +0900

    テスト実装中。

--- GitHubContributeWatcher/src/test/java/tools/gitHubcontributewatcher/service/GitHubServiceTest.java
+++ GitHubContributeWatcher/src/test/java/tools/gitHubcontributewatcher/service/GitHubServiceTest.java
@@ -4,11 +4,14 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertNotEquals;
+
 public class GitHubServiceTest {
     @Test
     public void getContributeListTest() throws Exception {
         GitHubService service = new GitHubService();
         List<String> list = service.getContributeList();
 
+        assertNotEquals(0, list.size());
     }
 }
