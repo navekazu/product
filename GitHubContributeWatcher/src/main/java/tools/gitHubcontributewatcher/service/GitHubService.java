83b37882b1b85e9c63b9420291a80338575a6e9f
navekazu <navekazu0927@gmail.com>
2017-06-01 07:32:43 +0900

    サービス化。

--- GitHubContributeWatcher/src/main/java/tools/gitHubcontributewatcher/service/GitHubService.java
+++ GitHubContributeWatcher/src/main/java/tools/gitHubcontributewatcher/service/GitHubService.java
@@ -1,8 +1,11 @@
 package tools.githubcontributewatcher.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class GitHubService {
     public List<String> getContributeList() {
         List<String> list = new ArrayList<>();
