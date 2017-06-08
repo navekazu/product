0f0dc3ca5322578e8cb0106ecf243cca4eb04211
navekazu <navekazu0927@gmail.com>
2017-06-09 07:34:13 +0900

    RestAPI用。

--- GitHubContributeWatcher/src/main/java/tools/gitHubcontributewatcher/api/GitHubApi.java
+++ GitHubContributeWatcher/src/main/java/tools/gitHubcontributewatcher/api/GitHubApi.java
@@ -2,6 +2,11 @@ package tools.githubcontributewatcher.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class GitHubApi {
+    public List<String> getContributeList() {
+        return null;
+    }
 }
