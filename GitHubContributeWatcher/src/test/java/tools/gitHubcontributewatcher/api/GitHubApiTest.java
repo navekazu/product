931a6b14a1f7502669e7b0491b0407dee51df64e
navekazu <navekazu0927@gmail.com>
2017-06-15 07:35:35 +0900

    RestAPI用。

--- GitHubContributeWatcher/src/test/java/tools/gitHubcontributewatcher/api/GitHubApiTest.java
+++ GitHubContributeWatcher/src/test/java/tools/gitHubcontributewatcher/api/GitHubApiTest.java
@@ -1,10 +1,12 @@
 package tools.githubcontributewatcher.api;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class GitHubApiTest {
+    @Autowired
     private GitHubApi api;
 
     @Test
