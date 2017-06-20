89d6ad40c75437e83cd7efd83edfb83d54b10520
navekazu <navekazu0927@gmail.com>
2017-06-21 00:00:24 +0900

    サービスのテスト

--- GitHubContributeWatcher/src/test/java/tools/gitHubcontributewatcher/service/GitHubServiceTest.java
+++ GitHubContributeWatcher/src/test/java/tools/gitHubcontributewatcher/service/GitHubServiceTest.java
@@ -1,15 +1,19 @@
 package tools.githubcontributewatcher.service;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 import static org.junit.Assert.assertNotEquals;
 
 public class GitHubServiceTest {
+    @Autowired
+    private GitHubService service;
+
     @Test
     public void getContributeListTest() throws Exception {
-        GitHubService service = new GitHubService();
+        service = new GitHubService();
         List<String> list = service.getContributeList();
 
         assertNotEquals(0, list.size());
