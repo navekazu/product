9259e505b1f43841a5b492c6a1fc85f1dfb3017c
navekazu <navekazu0927@gmail.com>
2017-06-22 07:28:21 +0900

    RestAPIの実装

--- GitHubContributeWatcher/src/main/java/tools/gitHubcontributewatcher/api/GitHubApi.java
+++ GitHubContributeWatcher/src/main/java/tools/gitHubcontributewatcher/api/GitHubApi.java
@@ -1,11 +1,16 @@
 package tools.githubcontributewatcher.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import tools.githubcontributewatcher.service.GitHubService;
 
 import java.util.List;
 
 @RestController
 public class GitHubApi {
+    @Autowired
+    private GitHubService gitHubService;
+
     public List<String> getContributeList() {
         return null;
     }
