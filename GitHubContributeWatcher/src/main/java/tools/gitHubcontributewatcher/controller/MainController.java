ccc50fecdfaac9ffd5e479dd94fe3db25622a25f
navekazu <navekazu0927@gmail.com>
2017-06-23 23:30:25 +0900

    コントローラの実装。

--- GitHubContributeWatcher/src/main/java/tools/gitHubcontributewatcher/controller/MainController.java
+++ GitHubContributeWatcher/src/main/java/tools/gitHubcontributewatcher/controller/MainController.java
@@ -1,7 +1,11 @@
 package tools.githubcontributewatcher.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import tools.githubcontributewatcher.service.GitHubService;
 
 @Controller
 public class MainController {
+    @Autowired
+    private GitHubService gitHubService;
 }
