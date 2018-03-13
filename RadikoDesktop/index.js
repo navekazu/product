1ab85a4e177c3876deb4b6d373bfc6aaf6babc40
k-watanabe <navekazu0927@gmail.com>
2018-03-13 10:19:41 +0900

    RadikoDesktop

---
+++ RadikoDesktop/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+app.on('ready', function() {
+
+  // ブラウザ(Chromium)の起動, 初期画面のロード
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('http://radiko.jp/');
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate());
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
