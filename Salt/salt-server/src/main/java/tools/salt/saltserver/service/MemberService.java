7f4d1a8446da6959a906f9805260b7a660f7b5d3
navekazu <navekazu0927@gmail.com>
2019-02-04 18:01:25 +0900

    諸々

---
+++ Salt/salt-server/src/main/java/tools/salt/saltserver/service/MemberService.java
@@ -0,0 +1,31 @@
+package tools.salt.saltserver.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import tools.salt.domain.Member;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    public Member getMember(Integer memberId) {
+        return null;
+    }
+    public Member getMember(String mailAddress, String password) {
+        return null;
+    }
+
+    @Transactional(readOnly = false)
+    public Member addMember(Member member) {
+        return member;
+    }
+    @Transactional(readOnly = false)
+    public Member updateMember(Integer memberId, Member member) {
+        return member;
+    }
+    @Transactional(readOnly = false)
+    public Member deleteMember(Integer memberId) {
+        return null;
+    }
+}
