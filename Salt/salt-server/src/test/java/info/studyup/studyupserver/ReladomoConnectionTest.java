94114d5c70a5e410c77ec182a7591ca96091d155
navekazu <navekazu0927@gmail.com>
2019-04-05 19:34:52 +0900

    諸々

--- Salt/salt-server/src/test/java/info/studyup/studyupserver/ReladomoConnectionTest.java
+++ Salt/salt-server/src/test/java/info/studyup/studyupserver/ReladomoConnectionTest.java
@@ -1,5 +1,23 @@
 package info.studyup.studyupserver;
 
-public class ReladomoConnectionTest {
+import org.junit.Before;
 
+import com.gs.fw.common.mithra.test.ConnectionManagerForTests;
+import com.gs.fw.common.mithra.test.MithraTestResource;
+
+public class ReladomoConnectionTest {
+    private String testRuntimeConfigXML = "MithraRuntimeConfig_test.xml";
+
+    @Before
+    public void setup() throws Exception {
+        intializeReladomoForTest();
+        initializeApp();
+    }
+
+    private void intializeReladomoForTest() {
+        MithraTestResource testResource = new MithraTestResource(testRuntimeConfigXML);
+        ConnectionManagerForTests connectionManager = ConnectionManagerForTests.getInstance("test");
+        testResource.createSingleDatabase(connectionManager, "testconfig/SimpleBankTestData.txt");
+        testResource.setUp();
+    }
 }
