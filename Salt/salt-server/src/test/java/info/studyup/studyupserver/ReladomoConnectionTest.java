89c4b7a45fbd2f55500105687f9d2b3fd7de4519
navekazu <navekazu0927@gmail.com>
2019-04-08 17:23:14 +0900

    諸々

--- Salt/salt-server/src/test/java/info/studyup/studyupserver/ReladomoConnectionTest.java
+++ Salt/salt-server/src/test/java/info/studyup/studyupserver/ReladomoConnectionTest.java
@@ -1,23 +1,37 @@
 package info.studyup.studyupserver;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import com.gs.fw.common.mithra.test.ConnectionManagerForTests;
 import com.gs.fw.common.mithra.test.MithraTestResource;
 
 public class ReladomoConnectionTest {
     private String testRuntimeConfigXML = "MithraRuntimeConfig_test.xml";
+    private MithraTestResource testResource;
 
     @Before
     public void setup() throws Exception {
         intializeReladomoForTest();
-        initializeApp();
+        // initializeApp();
     }
 
-    private void intializeReladomoForTest() {
-        MithraTestResource testResource = new MithraTestResource(testRuntimeConfigXML);
-        ConnectionManagerForTests connectionManager = ConnectionManagerForTests.getInstance("test");
+    @After
+    public void tearDown() throws Exception {
+        testResource.tearDown();
+    }
+
+    private void intializeReladomoForTest() throws Exception {
+        testResource = new MithraTestResource(testRuntimeConfigXML);
+        ConnectionManagerForTests connectionManager = ConnectionManagerForTests.getInstance("test_db");
         testResource.createSingleDatabase(connectionManager, "testconfig/SimpleBankTestData.txt");
+        testResource.createDatabaseForStringSourceAttribute(connectionManager, "deskA", "mithraTestDataForDesk.txt");
         testResource.setUp();
+    }
+
+    @Test
+    public void test() {
+
     }
 }
