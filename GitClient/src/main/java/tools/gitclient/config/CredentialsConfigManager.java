22d466c9dcfae0456e4051d50ef61859d4905b6c
navekazu <navekazu0927@gmail.com>
2020-02-25 09:57:41 +0900

    修正

--- GitClient/src/main/java/tools/gitclient/config/CredentialsConfigManager.java
+++ GitClient/src/main/java/tools/gitclient/config/CredentialsConfigManager.java
@@ -1,6 +1,11 @@
 package tools.gitclient.config;
 
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
 import javax.crypto.spec.IvParameterSpec;
+
 
 public class CredentialsConfigManager extends ConfigManagerBase {
 
@@ -28,10 +33,63 @@ public class CredentialsConfigManager extends ConfigManagerBase {
             NONE,
             USER_PASSWORD,
         }
+        public int no;
         public String name;
         public Type type;
         public String user;
         public String password;
+        public String toString() {
+            return String.format("%d,%s,%s,%s,%s", no, name, type.name(), user, password);
+        }
     }
 
+    public static String credentialsToBase64(Credentials credentials) {
+        return String.format("%d,%s,%s,%s,%s",
+                credentials.no, base64Encode(credentials.name), credentials.type.name(), base64Encode(credentials.user), base64Encode(credentials.password));
+    }
+
+    public static Credentials base64ToCredentials(String value) {
+        Credentials credentials = new Credentials();
+        String values[] = split(value, ",");
+        credentials.no = Integer.parseInt(values[0]);
+        credentials.name = base64Decode(values[1]);
+        credentials.type = Credentials.Type.valueOf(values[2]);
+        credentials.user = base64Decode(values[3]);
+        credentials.password = base64Decode(values[4]);
+
+        return credentials;
+    }
+
+    public static String[] split(String value, String delimiter) {
+        if (value.length()==0) {
+            return new String[0];
+        }
+
+        int from = 0;
+        int to = 1;
+        List<String> list = new ArrayList<>();
+
+        while(from<=value.length()) {
+            if (value.indexOf(delimiter, to)==-1) {
+                to = value.length();
+            } else {
+                to = value.indexOf(delimiter, to);
+            }
+            list.add(value.substring(from, to));
+            from = to+1;
+            to = from+1;
+        }
+
+        return list.toArray(new String[list.size()]);
+    }
+
+    public static String base64Decode(String value) {
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] b = decoder.decode(value);
+        return new String(b);
+    }
+    public static String base64Encode(String value) {
+        Base64.Encoder encoder = Base64.getEncoder();
+        return encoder.encodeToString(value.getBytes());
+    }
 }
---
+++ GitClient/src/test/java/tools/gitclient/config/CredentialsConfigManagerTest.java
@@ -0,0 +1,89 @@
+package tools.gitclient.config;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CredentialsConfigManagerTest {
+
+
+    @Test
+    public void credentialsTest() {
+        CredentialsConfigManager.Credentials credentials = new CredentialsConfigManager.Credentials();
+        credentials.no = 1;
+        credentials.name = "name";
+        credentials.type = CredentialsConfigManager.Credentials.Type.USER_PASSWORD;
+        credentials.user = "user";
+        credentials.password = "password";
+        assertThat(credentials.toString(), is("1,name,USER_PASSWORD,user,password"));
+    }
+
+    @Test
+    public void base64Test() {
+        assertThat(CredentialsConfigManager.base64Encode("abc"), is("YWJj"));
+        assertThat(CredentialsConfigManager.base64Encode("日本語"), is("5pel5pys6Kqe"));
+        assertThat(CredentialsConfigManager.base64Encode("comma,カンマ"), is("Y29tbWEs44Kr44Oz44Oe"));
+
+        assertThat(CredentialsConfigManager.base64Decode("YWJj"), is("abc"));
+        assertThat(CredentialsConfigManager.base64Decode("5pel5pys6Kqe"), is("日本語"));
+        assertThat(CredentialsConfigManager.base64Decode("Y29tbWEs44Kr44Oz44Oe"), is("comma,カンマ"));
+    }
+
+    @Test
+    public void credentialsSerializeTest() {
+        CredentialsConfigManager.Credentials credentials = new CredentialsConfigManager.Credentials();
+        credentials.no = 1;
+        credentials.name = "name";
+        credentials.type = CredentialsConfigManager.Credentials.Type.USER_PASSWORD;
+        credentials.user = "user";
+        credentials.password = "password";
+
+        assertThat(CredentialsConfigManager.credentialsToBase64(credentials),
+                is("1,bmFtZQ==,USER_PASSWORD,dXNlcg==,cGFzc3dvcmQ="));
+
+        credentials = CredentialsConfigManager.base64ToCredentials("1,bmFtZQ==,USER_PASSWORD,dXNlcg==,cGFzc3dvcmQ=");
+        assertThat(credentials.no, is(1));
+        assertThat(credentials.name, is("name"));
+        assertThat(credentials.type, is(CredentialsConfigManager.Credentials.Type.USER_PASSWORD));
+        assertThat(credentials.user, is("user"));
+        assertThat(credentials.password, is("password"));
+
+
+        credentials = new CredentialsConfigManager.Credentials();
+        credentials.no = 1;
+        credentials.name = "name";
+        credentials.type = CredentialsConfigManager.Credentials.Type.NONE;
+        credentials.user = "";
+        credentials.password = "";
+
+        assertThat(CredentialsConfigManager.credentialsToBase64(credentials),
+                is("1,bmFtZQ==,NONE,,"));
+
+        credentials = CredentialsConfigManager.base64ToCredentials("1,bmFtZQ==,NONE,,");
+        assertThat(credentials.no, is(1));
+        assertThat(credentials.name, is("name"));
+        assertThat(credentials.type, is(CredentialsConfigManager.Credentials.Type.NONE));
+        assertThat(credentials.user, is(""));
+        assertThat(credentials.password, is(""));
+    }
+
+    @Test
+    public void splitTest() {
+        String[] values = CredentialsConfigManager.split("0,1,2,3", ",");
+
+        assertThat(values.length, is(4));
+        assertThat(values[0], is("0"));
+        assertThat(values[1], is("1"));
+        assertThat(values[2], is("2"));
+        assertThat(values[3], is("3"));
+
+        values = CredentialsConfigManager.split("0,1,,", ",");
+
+        assertThat(values.length, is(4));
+        assertThat(values[0], is("0"));
+        assertThat(values[1], is("1"));
+        assertThat(values[2], is(""));
+        assertThat(values[3], is(""));
+    }
+}
