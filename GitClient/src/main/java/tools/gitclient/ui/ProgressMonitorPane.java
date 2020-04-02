fc8599055b287bfa935b87623cc5d7c7d21b9870
navekazu <navekazu0927@gmail.com>
2020-04-02 09:59:39 +0900

    修正

--- GitClient/src/main/java/tools/gitclient/ui/ProgressMonitorPane.java
+++ GitClient/src/main/java/tools/gitclient/ui/ProgressMonitorPane.java
@@ -1,5 +1,8 @@
 package tools.gitclient.ui;
 
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
 import org.eclipse.jgit.lib.BatchingProgressMonitor;
 
 import tools.gitclient.OperationMessage;
@@ -7,51 +10,45 @@ import tools.gitclient.OperationMessage;
 public class ProgressMonitorPane extends BatchingProgressMonitor implements AutoCloseable {
     private OperationMessage operationMessage;
     private String title;
-    private javax.swing.ProgressMonitor progressMonitor;
+    private JOptionPane optionPane;
+    private JDialog dialog;
     private boolean cancelled = false;
 
     public ProgressMonitorPane(String title, OperationMessage operationMessage) {
         this.title = title;
         this.operationMessage = operationMessage;
-        progressMonitor = new javax.swing.ProgressMonitor(
-                operationMessage.getMainFrame(), title, "", 0, 10);
-        progressMonitor.setMillisToPopup(0);
+        this.optionPane = new JOptionPane();
+        dialog = optionPane.createDialog(title);
+        dialog.setModal(false);
+        dialog.setVisible(true);
     }
 
     public void cancel() {
         cancelled = true;
-        if (progressMonitor!=null) {
-            progressMonitor.close();
-        }
     }
 
     @Override
     protected void onUpdate(String taskName, int workCurr) {
-        progressMonitor.setNote(taskName);
+        optionPane.setMessage(taskName);
     }
 
     @Override
     protected void onEndTask(String taskName, int workCurr) {
-        progressMonitor.setNote(taskName);
     }
 
     @Override
     protected void onUpdate(String taskName, int workCurr, int workTotal, int percentDone) {
-        progressMonitor.setNote(taskName);
-        progressMonitor.setMaximum(workTotal);
-        progressMonitor.setProgress(workCurr);
+        optionPane.setMessage(taskName);
     }
 
     @Override
     protected void onEndTask(String taskName, int workCurr, int workTotal, int percentDone) {
-        progressMonitor.setNote(taskName);
-        progressMonitor.setMaximum(workTotal);
-        progressMonitor.setProgress(workCurr);
     }
 
     @Override
     public void close() throws Exception {
         // TODO 自動生成されたメソッド・スタブ
-        progressMonitor.close();
+        dialog.setVisible(false);
+        dialog.dispose();
     }
 }
